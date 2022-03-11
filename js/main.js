1947b9b166fa308eed8091ae51dd1465bca8d975
Chris Ahn <chrisahn90@yahoo.com>
2022-03-10 17:23:19 -0800

    Completed user can search

--- js/main.js
+++ js/main.js
@@ -1,25 +1,61 @@
-
 var targetUrl = encodeURIComponent('https://www.fishwatch.gov/api/species');
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
-xhr.setRequestHeader('token', 'abc123');
-xhr.responseType = 'json';
-xhr.addEventListener('load', function () {
-  // console.log(xhr.response);
-  for (var i = 0; i < xhr.response.length; i++) {
-    console.log('Species Name: ', xhr.response[i]['Species Name']);
-    // console.log('Scientific Name: ', xhr.response[i]['Scientific Name']);
-    // console.log('Population: ', xhr.response[i].Population);
-    // console.log('Environmental Concerns: ', xhr.response[i]['Environmental Concerns']);
-    // console.log('Environmental Effects: ', xhr.response[i]['Environmental Effects']);
 
-    // console.log('Total Fat: ', xhr.response[i]['Total Fat']);
-    // console.log('Carbohydrates: ', xhr.response[i].Carbohydrates);
-    // console.log('Calories: ', xhr.response[i].Calories);
-    // console.log('Cholesterol: ', xhr.response[i].Cholesterol);
-    // console.log('Protein: ', xhr.response[i].Protein);
-  }
+var search = document.querySelector('.search-button');
+var input = document.querySelector('#search-results-form').elements;
 
-});
-xhr.send();
+search.addEventListener('click', resultSpecies);
+
+function resultSpecies(event) {
+  event.preventDefault();
+  xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+  xhr.setRequestHeader('token', 'abc123');
+  xhr.responseType = 'json';
+
+  xhr.addEventListener('load', function () {
+    var arr = [];
+    var ul = document.querySelector('.results');
+    for (var i = 0; i < xhr.response.length; i++) {
+      arr = xhr.response[i]['Species Name'].split(' ');
+      if (arr.includes(input[0].value)) {
+        ul.appendChild(appendList(xhr.response[i]));
+        continue;
+      }
+      if (input[0].value === xhr.response[i]['Species Name']) {
+        ul.appendChild(appendList(xhr.response[i]));
+        continue;
+      }
+    }
+
+  });
+  xhr.send();
+}
+
+function appendList(species) {
+  var li = document.createElement('li');
+  li.setAttribute('class', 'species-box');
+
+  var image = document.createElement('div');
+  image.setAttribute('class', 'species-image');
+
+  var img = document.createElement('img');
+  img.src = species['Species Illustration Photo'].src;
+
+  image.appendChild(img);
+
+  var name = document.createElement('p');
+  name.setAttribute('class', 'name');
+
+  name.textContent = species['Species Name'];
+
+  var alias = document.createElement('a');
+  alias.setAttribute('href', species['Species Aliases'].href);
+
+  li.appendChild(image);
+  li.appendChild(name);
+  li.appendChild(alias);
+
+  return li;
+
+}
